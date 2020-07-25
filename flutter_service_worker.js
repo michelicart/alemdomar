a1d5dba74979d7328b6991247211093eaf157a98
Michel Icart <michel.c.icart@gmail.com>
2020-07-25 09:54:03 -0300

    final

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,26 +5,30 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "b1eb6dc79c9bcfb75e43da3653f2ee6a",
 "/": "b1eb6dc79c9bcfb75e43da3653f2ee6a",
-"main.dart.js": "9c7ced73ef6ee68938d0f50310002c0e",
+"main.dart.js": "296bdf829246146b84ef8ded84b9f7ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "1094fc05d9c9a690a65270f92a698342",
-"assets/images/Mockups/livro.png": "67db0aa898d72b2084a4b3a722019404",
+"assets/images/Mockups/livro.png": "c6decd15a8411b6f9c4e159bf72ec7c4",
+"assets/images/Mockups/capa_low.jpg": "f295e5b08c05665faa6cd6f1141d1699",
 "assets/images/Mockups/alemdomar1%2520copy.png": "326471b9a146cefe52f8cf1d2531b05d",
 "assets/images/Mockups/Aberto_singapura.jpg": "253f2442dbef316f6be19d458bdcd4af",
 "assets/images/Mockups/Aberto_New%2520Zealand.jpg": "140a28db85a11987a225585c8a06d225",
 "assets/images/Mockups/alemdomar1.png": "11dc609076cf3e60b0ea775c4014381f",
 "assets/images/Mockups/Aberto_seoul.jpg": "598c7c29941d2d28c00e932e046bc6e9",
 "assets/images/Mockups/alem.jpg": "049a199a419f5e24681643520726a3ef",
-"assets/images/Mockups/Capa%2520copy.jpg": "8a173d81b08af6e6d431a6fb0da91eeb",
+"assets/images/Mockups/Capa%2520copy.jpg": "c03d2c582058628e3394355c3fce6221",
 "assets/images/Mockups/Aberto_bayofislands.jpg": "cf7b6db8e68d160a20b44e5ba28fc565",
 "assets/images/Mockups/Capa.jpg": "d474e24b07da263e65672af0ef1152e9",
-"assets/AssetManifest.json": "323fddd331177105514b0c79c108d398",
+"assets/images/fav.png": "e3a1f27b45c0a76634e18d40ca5dd36c",
+"assets/AssetManifest.json": "8784789d7b32e6cb135d81a0b15ba4ca",
 "assets/NOTICES": "108d0a1640e6b622b04c79e5a1de9f51",
-"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"assets/FontManifest.json": "cea94a5d24362297d2164fa77e55e73d",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"assets/assets/fonts/Bariol_Regular.otf": "ffe84263f982cbe9bae053b0cbb6dcda",
+"assets/assets/fonts/Bariol_Light.otf": "bc003c9f8669b675919825cf8cba41d5"
 };
 
 // The application shell files that are downloaded before a service worker can
